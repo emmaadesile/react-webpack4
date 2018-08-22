1799fdb655e8909226deb3c7e65cdced6b2ba6dc
Emmanuel Adesile <emma2adesile@gmail.com>
2018-08-22 03:04:09 +0100

    Setup webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HTMLWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
